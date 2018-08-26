a53b015a3176a9a21250cacd0334056cc056e190
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-08-27 03:03:18 +0700

    Add Register of Hospital

--- ElderlyCare/app/src/main/java/com/jenny/elderlycare/RegisterActivity.java
+++ ElderlyCare/app/src/main/java/com/jenny/elderlycare/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.jenny.elderlycare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void registerHospital(View view) {
+        Intent intent = new Intent(RegisterActivity.this, RegisterHospitalActivity.class);
+        startActivity(intent);
     }
 }
